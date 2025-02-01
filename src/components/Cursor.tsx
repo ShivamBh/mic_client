6b67fcde0aef2f0b3650d012416172b5fa171249
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2025-02-01 22:02:09 +0530

    fix: cursor size mobile conditional

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,9 +1,12 @@
+import { useMediaQuery } from "react-responsive";
+
 function CursorSvg() {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 800px)' })
   return (
     <>
       <img 
-        width={80}
-        height={80}
+        width={isTabletOrMobile ? 85 : 80}
+        height={isTabletOrMobile ? 85 : 80}
       src={`${new URL("../assets/cursor/MicrorestCursor.svg", import.meta.url).href}`} alt="" />
     </>
   );
