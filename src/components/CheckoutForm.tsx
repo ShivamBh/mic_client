30a203555ea7138544312127c6df68f924e2e385
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2026-05-16 10:40:20 +0530

    fix: added confirmParams requried field

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -65,7 +65,11 @@ const CheckoutForm = () => {
     const { error } = await stripe.confirmPayment({
       elements,
       clientSecret: clientSecret,
+
       redirect: 'if_required',
+      confirmParams: {
+        return_url: import.meta.env.VITE_SITE_URL,
+      },
     });
 
     if (error) {
